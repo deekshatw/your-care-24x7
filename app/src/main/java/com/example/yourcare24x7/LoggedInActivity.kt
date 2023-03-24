88d7f0f636dbdb3c97ec8772dbc4fc0e551d4808
Krrish Kamal <aryakrrish02@gmail.com>
2023-03-24 16:38:03 +0530

    Created logged in activity and logged out activity

--- app/src/main/java/com/example/yourcare24x7/LoggedInActivity.kt
+++ app/src/main/java/com/example/yourcare24x7/LoggedInActivity.kt
@@ -1,11 +1,18 @@
 package com.example.yourcare24x7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoggedInActivity : AppCompatActivity() {
+    private lateinit var continueBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
+        continueBtn = findViewById(R.id.conitnueBtn)
+        continueBtn.setOnClickListener {
+            startActivity(Intent(this@LoggedInActivity,FeelingsActivity::class.java))
+        }
     }
 }
